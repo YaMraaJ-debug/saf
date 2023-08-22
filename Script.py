dc63d213b5cc03e2fa7615d5182ec49e2ae20e9a
Botprim <123348827+Botprim@users.noreply.github.com>
2023-08-22 13:02:02 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,7 +4,7 @@ class script(object):
 ɪ ᴀᴍ ᴘᴏᴡᴇʀ ғᴜʟʟ ᴀᴜᴛᴏ ғɪʟᴛᴇʀ ʙᴏᴛ
 ɪ ᴀᴍ ᴘᴍ sʜᴏʀᴛɴᴇʀ-ʟɪɴᴋ ʙᴏᴛ
 
-ᴍᴀɴᴛᴀɪɴᴇᴅ ʙʏ : <a href="https://t.me/Sivam_uv">♥⁠╣⁠[⁠-⁠_⁠-⁠]⁠╠⁠♥Creation♥⁠╣⁠[⁠-⁠_⁠-⁠]⁠╠⁠♥</a></b> 😏</b>"""
+ᴍᴀɴᴛᴀɪɴᴇᴅ ʙʏ : <a href="https://t.me/Sivam_uv">♥⁠╣Creation╠⁠♥</a></b> 😏</b>"""
 
     
     HELP_TXT = """<b>Hᴇʏ {}
