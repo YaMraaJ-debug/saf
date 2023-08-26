c4c1009461a6e88d9d28013eaf6eb2d1319e094e
Botprim <123348827+Botprim@users.noreply.github.com>
2023-08-26 10:53:08 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -18,7 +18,7 @@ from aiohttp import web
 from aiohttp.http_exceptions import BadStatusLine
 from lazybot import multi_clients, work_loads, LazyPrincessBot
 from server.exceptions import FIleNotFound, InvalidHash
-from zzint import StartTime, __version__
+from Kismis import StartTime, __version__
 from util.custom_dl import ByteStreamer
 from util.time_format import get_readable_time
 from util.render_template import render_page
