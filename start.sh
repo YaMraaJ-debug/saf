2bc72f2c26daf7dcadaf5d0b8cfcfe43764810a8
Botprim <123348827+Botprim@users.noreply.github.com>
2023-08-27 09:10:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,12 @@
-gunicorn app:app & python3 bot.py
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/lazyindu/wowbot.git /LazyPrincess
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /LazyPrincess
+fi
+cd /LazyPrincess
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
